b71192a84f9f3cfb3a800adda942c1f1100ace24
hui0419 <3024959127@qq.com>
2024-10-08 11:43:51 +0800

    项目初始化

---
+++ src/main/java/com/hui/huiojcodesandbox/HuiojCodeSandboxApplication.java
@@ -0,0 +1,13 @@
+package com.hui.huiojcodesandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuiojCodeSandboxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HuiojCodeSandboxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hui/huiojcodesandbox/HuiojCodeSandboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hui.huiojcodesandbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HuiojCodeSandboxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
